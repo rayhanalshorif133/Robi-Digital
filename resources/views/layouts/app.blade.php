3176b618bc1f8c594dd323cd161d733e2aa2aa13
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-09-18 12:05:14 +0600

    logout btn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,8 +51,8 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-                height="60" width="60">
+            <img class="animation__shake" src="{{ asset('assets/images/robi-logo.png') }}" alt="AdminLTELogo"
+                height="60" width="auto">
         </div>
 
         <!-- Navbar -->
