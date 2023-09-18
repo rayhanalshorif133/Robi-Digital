7ac85e805f5a0a697076634c4dce52c245f72ef6
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-09-18 12:08:36 +0600

    dasboard done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HitLog;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $hitLogs = HitLog::count();
+        $services = Service::count();
+        return view('home', compact('services', 'hitLogs'));
     }
 }
